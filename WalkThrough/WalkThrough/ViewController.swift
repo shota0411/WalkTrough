2a53aa300130f1312a9108b84b9026805b757d6e
松田 翔大 <thank2626@gmail.com>
2018-05-08 20:48:43 +0900

    新規プロジェクトの作成

---
+++ WalkThrough/WalkThrough/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WalkThrough
+//
+//  Created by 松田翔大 on 2018/05/08.
+//  Copyright © 2018年 松田翔大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
